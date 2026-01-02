f66b9cf935864213542ae649700ddee781156bd9
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2026-01-02 12:10:50 -0700

    todos router

--- todo/main.py
+++ todo/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-import models
-from database import engine
+from todo import models
+from todo.database import engine
+from todo.routes import auth, todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+app.include_router(auth.router)
+app.include_router(todos.router)
